6b78bc68d3f0e67ad4524ffe4c6247c767b5d137
Gabrielle Baptista da Silva Fonseca <gabriellebsf@gmail.com>
2020-04-17 13:57:49 -0300

    Classe Carro pronta

---
+++ src/main/java/Computador.java
@@ -0,0 +1,56 @@
+public class Computador {
+        private String idComputador;
+        private String marcaComputador;
+        private String processador;
+        private String qtdRAM;
+        private String tamanhoDisco;
+
+        public Computador() {}
+
+        public Computador(String idComputador, String marcaComputador, String processador, String qtdRAM, String tamanhoDisco) {
+            this.setIdComputador (idComputador);
+            this.setMarcaComputador (marcaComputador);
+            this.setProcessador (processador);
+            this.setQtdRAM (qtdRAM);
+            this.setTamanhoDisco (tamanhoDisco);
+        }
+
+        public String getIdComputador() {
+            return idComputador;
+        }
+        public void setIdComputador(String idComputador) { this.idComputador = idComputador; }
+
+        public String getMarcaComputador() {
+            return marcaComputador;
+        }
+        public void setMarcaComputador(String marcaComputador) {
+            this.marcaComputador = marcaComputador;
+        }
+
+        public String getProcessador() {
+            return processador;
+        }
+        public void setProcessador(String marca) {
+            this.processador = processador;
+        }
+
+        public String getQtdRAM() {
+            return qtdRAM;
+        }
+        public void setQtdRAM(String qtdRAM) {
+            this.qtdRAM = qtdRAM;
+        }
+
+        public String getTamanhoDisco() {
+            return tamanhoDisco;
+        }
+        public void setTamanhoDisco(String tamanhoDisco) {
+            this.tamanhoDisco = tamanhoDisco;
+        }
+
+        @Override
+        public String toString() {
+            return "Número serial do computador: " + getIdComputador() + "[Processador: " + processador + ", Marca do computador: " +
+                    marcaComputador + ", Quantidade de memória RAM: " + qtdRAM + ", Tamanho do disco: " + tamanhoDisco + "]";
+        }
+    }
